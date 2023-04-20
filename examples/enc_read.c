15d9e45e89946bd3f1a1fd763359aaeff9bff3c8
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2023-04-20 12:49:26 +0200

    Normalize examples

--- examples/enc_read.c
+++ examples/enc_read.c
@@ -6,27 +6,28 @@
 
 #include "h_time.h"
 
-cbEncoder_t cbEncoderLeft = {GPIO_PIN_NC, 0, 0, 0};
-cbEncoder_t cbEncoderRight = {GPIO_PIN_NC, 0, 0, 0};
+cbEncoder_t cbEncoderLeft = {PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B, GPIO_PIN_NC, 0, 0, 0};
+cbEncoder_t cbEncoderRight = {PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B, GPIO_PIN_NC, 0, 0, 0};
 
 void init() {
     if (gpioInitialise() < 0) exit(EXIT_FAILURE);
     // Left
-    cbEncoderGPIOinit(PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B);
+    cbEncoderGPIOinit(&cbEncoderLeft);
     cbEncoderRegisterISRs(&cbEncoderLeft, PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B, 50);
     // Right
-    cbEncoderGPIOinit(PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B);
+    cbEncoderGPIOinit(&cbEncoderRight);
     cbEncoderRegisterISRs(&cbEncoderRight, PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B, 50);
 }
 
 void terminate() {
-    cbEncoderCancelISRs(PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B);
-    cbEncoderCancelISRs(PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B);
+    cbEncoderCancelISRs(&cbEncoderLeft);
+    cbEncoderCancelISRs(&cbEncoderRight);
     gpioTerminate();
-    exit(EXIT_SUCCESS);
 }
+
 int main(void) {
     init();
+    atexit(terminate);
     int delta_ms = 500;
     printf("Every %dms:\n", delta_ms);
     timespec_t tick;
@@ -39,6 +40,5 @@ int main(void) {
         printf("L:%d, R:%d\n", cbEncoderLeft.direction, cbEncoderRight.direction);
         HTime_InitBase();
     }
-    terminate();
     exit(EXIT_SUCCESS);
 }
