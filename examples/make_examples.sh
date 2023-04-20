f9a2144255777f12a91ca2d60990a58905850167
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2023-04-20 12:21:58 +0100

    Make example script refactoring

--- examples/make_examples.sh
+++ examples/make_examples.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc template.c -o template.$(uname -m) -L.. -l:libcoderbot.a -lpigpio
-gcc read_encoders.c -o read_encoders.$(uname -m) -L.. -l:libcoderbot.a -lpigpio
+for SRC in *.c; do
+    EXN=$(echo $SRC | cut -d '.' -f 1)
+    gcc $SRC -o $EXN.$(uname -m) -L.. -l:libcoderbot.a -lpigpio
+done
