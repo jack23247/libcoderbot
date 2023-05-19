a360b86dd473b59c77e7527fb108338904e24f30
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2023-05-19 11:37:53 +0200

    Refactored the P-D controller

--- examples/control.c
+++ examples/control.c
@@ -1,12 +1,41 @@
-#include <math.h>
+/**
+ * @file control.c
+ * @author Jacopo Maltagliati
+ * @date 19 Mag 2023
+ * @brief Trivial P-D controller for the CoderBot platform.
+ * @copyright Copyright (c) 2022-23, Jacopo Maltagliati.
+ * 
+ * This file is part of libcoderbot.
+ * 
+ * libcoderbot is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * libcoderbot is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * libcoderbot. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/* TODOs:
+ * - Separare raggi da altri componenti costante
+ * - Costanti separate LEFT e RIGHT
+ * - ~~Togliere denominatore calcolo errore~~ Fatto
+ */
+
 #include <pigpio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #include "../include/cbdef.h"
 #include "../include/motor.h"
 #include "../include/encoder.h"
 #include "timespec.h"
+
+/* PID PARAMETERS ---------------------------------------------------------- */
 
 #define PID_RESPONSE_MEDIUM
 
@@ -28,9 +57,30 @@
 #define KI 0.03  // integral coefficient
 #endif
 
-// PID parameters
 #define PI_INTERVAL_MSEC 20.0f // 50Hz
 #define ENC_DIST_PER_TICK_MM 0.06
+#define LEFT_WHEEL_RAY 60.f
+#define RIGHT_WHEEL_RAY 61.f
+
+/* TYPEDEFS ---------------------------------------------------------------- */
+
+/**
+ * @brief A structure holding the parameters for the P-I controller.
+ */
+typedef struct {
+    int ticks, //< The ticks read from the encoder.
+        prevTicks; //< The ticks read the previous time the structure was updated.
+    float dutyCyc, //< The duty cycle of the motor in percentage. // MAYBE const?
+          travel_mm, //< The distance the wheel has traveled in mm.
+          speed_mm_s, //< The speed of the wheel in mm.
+          targetSpeed_mm_s, //< The desired speed in mm/s.
+          error_mm_s, //< The error in speed in mm/s.
+          integralError_mm_s, //< The sum of the errors in mm/s.
+          correction; //< The correction factor in duty cycle percentage.
+    const float mmsPerTick; //< The distance traveled by the wheel per each tick in mm.
+} ctrlParams_t;
+
+/* GLOBALS ----------------------------------------------------------------- */
 
 cbMotor_t cbMotorLeft = {PIN_LEFT_FORWARD, PIN_LEFT_BACKWARD, forward};
 cbMotor_t cbMotorRight = {PIN_RIGHT_FORWARD, PIN_RIGHT_BACKWARD, forward};
@@ -38,6 +88,8 @@ cbEncoder_t cbEncoderLeft = {
     PIN_ENCODER_LEFT_A, PIN_ENCODER_LEFT_B, GPIO_PIN_NC, 0, 0, 0};
 cbEncoder_t cbEncoderRight = {
     PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B, GPIO_PIN_NC, 0, 0, 0};
+
+/* FUNCTIONS --------------------------------------------------------------- */
 
 void init() {
     if (gpioInitialise() < 0) exit(EXIT_FAILURE);
@@ -74,54 +126,78 @@ void printEncoderData(const cbEncoder_t* l, const cbEncoder_t* r) {
            r->bad_ticks);
 }
 
-void control() {
-    int distFromGoal_mm = 115;
-    int prevLeftTicks = 0;
-    int prevRightTicks = 0;
-    int leftError, rightError, leftIntegralError = 0, rightIntegralError = 0;
+void update(ctrlParams_t* p) {
+    //p->ticks = cbEncoderLeft.ticks;
+    p->travel_mm = (p->ticks - p->prevTicks) * p->ticksPerMm;
+    p->prevTicks = p->ticks;
+    p->speed_mm_s = p->travel_mm / (PI_INTERVAL_MSEC * MSEC_PER_SEC);
+    p->error_mm_s = (p->targetSpeed_mm_s - p->speed_mm_s); // / targetSpeed[L]_mm_s * 100.0;
+    p->correction = (p->error_mm_s * KP) + (p->integralError_mm_s * KI);
+    p->integralError_mm_s += p->error_mm_s;
+}
 
-    float targetSpeedLeft_mm_s = 5.0;
-    float targetSpeedRight_mm_s = 5.0;
+/**
+ * @brief Trivial Proportional-Integral controller for the CoderBot Platform.
+ * 
+ * @param distFromGoal_mm The distance from the goal in millimiters.
+ * @param targetSpeed_mm_s_L The target speed of the left wheel in mm/s.
+ * @param targetSpeed_mm_s_R The target speed of the right wheel in mm/s.
+ * @param dutyCycL The initial duty cycle of the left motor.
+ * @param dutyCycR The initial duty cycle of the right motor.
+ */
+void control(float distFromGoal_mm, float targetSpeed_mm_s_L, float targetSpeed_mm_s_R, float dutyCycL, float dutyCycR) {
 
-    // Assume that the initial duty cycles are equal and that coderbot
-    // moves at 11.5mm/s at full PWM duty cycle, it should reach its goal after
-    // 10 sec
-    float leftDutyCyc = 1.0;
-    float rightDutyCyc = 1.0;
+    ctrlParams_t left = {
+        .ticks = 0, 
+        .prevTicks = 0, 
+        .dutyCyc = dutyCycL, 
+        .travel_mm = 0.f, 
+        .speed_mm_s = 0.f,
+        .targetSpeed_mm_s = targetSpeed_mm_s_L,
+        .error_mm_s = 0,
+        .integralError_mm_s = 0,
+        .correction = 0,
+        .mmsPerTick = RIGHT_WHEEL_RAY * (360/16) // TODO da rivedere
+    };
 
-    cbMotorMove(&cbMotorLeft, forward, leftDutyCyc);
-    cbMotorMove(&cbMotorRight, forward, rightDutyCyc);
+    ctrlParams_t right = {
+        .ticks = 0, 
+        .prevTicks = 0, 
+        .dutyCyc = dutyCycR, 
+        .travel_mm = 0.f, 
+        .speed_mm_s = 0.f,
+        .targetSpeed_mm_s = targetSpeed_mm_s_R,
+        .error_mm_s = 0,
+        .integralError_mm_s = 0,
+        .correction = 0,
+        .mmsPerTick = RIGHT_WHEEL_RAY * (360/16) // TODO da rivedere
+    };
+
+    cbMotorMove(&cbMotorLeft, forward, left.dutyCyc);
+    cbMotorMove(&cbMotorRight, forward, right.dutyCyc);
 
     while (distFromGoal_mm > 0) {
+        /* XXX
+         * Copying the ticks should be done as quickly as possible to avoid an 
+         * ISR interrupt from happeining in between the copies. Consider 
+         * disabling the encoder callback temporarily if problems arise. 
+         */
+        left.ticks = cbEncoderLeft.ticks;
+        right.ticks = cbEncoderRight.ticks;
+        update(&left);
+        update(&right);
+        if(left.speed > 1.f && right.speed > 1.f) {
+            cbMotorMove(&cbMotorLeft, forward, left.dutyCyc + left.correction - right.correction);
+            cbMotorMove(&cbMotorLeft, forward, right.DutyCyc + right.correction - left.correction);
+        }
+        distFromGoal_mm -= (left.travel_mm + right.travel_mm) / 2;
         sleep(PI_INTERVAL_MSEC);
-        int leftTicks = cbEncoderLeft.ticks;
-        int rightTicks = cbEncoderRight.ticks;
-        float leftCorr, rightCorr;
-        int distLeft_mm = ENC_DIST_PER_TICK_MM * (leftTicks - prevLeftTicks);
-        {
-            prevLeftTicks = leftTicks;
-            float speedLeft_mm_s = distLeft_mm / (PI_INTERVAL_MSEC * MSEC_PER_SEC);
-            leftError = (targetSpeedLeft_mm_s - speedLeft_mm_s) / targetSpeedLeft_mm_s * 100.0;
-            leftCorr = (leftErr * KP) + (leftIntegralError * KI);
-            leftIntegralError += leftError;
-        }
-        int distRight_mm = ENC_DIST_PER_TICK_MM * (rightTicks - prevRightTicks);
-        {
-            prevRightTicks = rightTicks;
-            float speedRight_mm_s = distRight_mm / (PI_INTERVAL_MSEC * MSEC_PER_SEC);
-            leftError = (targetSpeedRight_mm_s - speedRight_mm_s) / targetSpeedRight_mm_s * 100.0;
-            rightCorr = (rightErr * KP) + (rightIntegralError * KI);
-            rightIntegralError += rightError;
-        }
-        distFromGoal_mm -= (distLeft_mm + distRight_mm) / 2;
-        cbMotorMove(&cbMotorLeft, forward, leftDutyCyc + leftCorr - rightCorr);
-        cbMotorMove(&cbMotorLeft, forward, rightDutyCyc + rightCorr - leftCorr);
     }
 }
 
 int main(void) {
     init();
     atexit(terminate);
-    control();
+    control(115.f, 10.0f, 10.0f, 1.0, 1.0);
     exit(EXIT_SUCCESS);
 }
