4bac7d82a249dce43b2691b07fcf7602d6941ad9
Jacopo Maltagliati <j.maltagliati@campus.unimib.it>
2023-06-06 19:46:47 +0200

    Fixed Real Time demo application

--- examples/rt_odo.c
+++ examples/rt_odo.c
@@ -1,5 +1,5 @@
 /**
- * @file rt.c
+ * @file rt_odo.c
  * @author Jacopo Maltagliati
  * @date 6 Giu 2023
  * @brief Skeleton of an RT controller for the CoderBot platform.
@@ -25,6 +25,17 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <errno.h>
+
+#include <linux/sched.h>
+#include <sched.h>
+#include <signal.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #include "../include/cbdef.h"
 #include "../include/motor.h"
@@ -33,11 +44,13 @@
 
 /* RT SCHEDULING PARAMETERS ------------------------------------------------ */
 
-#define MAX_TASKS 8 //< The maximum number of allowed tasks in the system
+#define UPTICK_RUNTIME 5 * NSEC_PER_MSEC //< Expected task runtime in ns
+#define UPTICK_PERIOD 7 * NSEC_PER_MSEC //< Task period in ns
+#define UPTICK_DEADLINE 6 * NSEC_PER_MSEC //< Task deadline in ns
 
-#define EXAMPLE_RUNTIME 10 * NSEC_PER_MSEC //< Expected task runtime in ns
-#define EXAMPLE_PERIOD 20 * NSEC_PER_MSEC //< Task period in ns
-#define EXAMPLE_DEADLINE 11 * NSEC_PER_MSEC //< Task deadline in ns
+#define ODO_RUNTIME 30 * NSEC_PER_MSEC //< Expected task runtime in ns
+#define ODO_PERIOD 40 * NSEC_PER_MSEC //< Task period in ns
+#define ODO_DEADLINE 31 * NSEC_PER_MSEC //< Task deadline in ns
 
 /* CB PARAMETERS ----------------------------------------------------------- */
 
@@ -46,9 +59,9 @@
 #define TICKS_PER_REVOLUTION 16 //< Ticks per motor revolution
 #define TRANSMISSION_RATIO 120
 
-#define DISTANCE_FROM_GOAL 100.f //< Distance from goal in mm
+#define DISTANCE_FROM_GOAL 500.f //< Distance from goal in mm
 
-#define DUTY_CYC_L .75f //< Duty cycle for the left wheel
+#define DUTY_CYC_L .5f //< Duty cycle for the left wheel
 #define DUTY_CYC_R DUTY_CYC_L //< Duty cycle for the right wheel
 
 /* TYPEDEFS ---------------------------------------------------------------- */
@@ -91,12 +104,13 @@ cbEncoder_t cbEncoderRight = {
     PIN_ENCODER_RIGHT_A, PIN_ENCODER_RIGHT_B, GPIO_PIN_NC, 0, 0, 0};
 
 volatile int ticks_L, ticks_R;
+volatile int halt = 0;
 
 pthread_mutex_t ticksMutex;
 
 /* FUNCTIONS --------------------------------------------------------------- */
 
-void init() {
+void cbInit() {
     if (gpioInitialise() < 0) exit(EXIT_FAILURE);
     // Left
     cbMotorGPIOinit(&cbMotorLeft);
@@ -108,7 +122,7 @@ void init() {
     cbEncoderRegisterISRs(&cbEncoderRight, 50);
 }
 
-void terminate() {
+void cbTerminate() {
     cbMotorReset(&cbMotorLeft);
     cbMotorReset(&cbMotorRight);
     cbEncoderCancelISRs(&cbEncoderLeft);
@@ -125,9 +139,10 @@ void terminate() {
  *
  * @return On success, 0; on failure -1. The error code is stored in errno
  */
-static inline int sched_setattr(
-    pid_t pid, const struct sched_attr* attr,
-    unsigned int flags) {  // TODO who knows what "flags" does? Is
+static inline int sched_setattr(pid_t pid, 
+								const struct sched_attr* attr, 
+								unsigned int flags) {  
+    // TODO who knows what "flags" does? Is
     // it the same as "sched_attr.flags"?
     return syscall(__NR_sched_setattr, pid, attr, flags);
 }
@@ -140,8 +155,10 @@ static inline int sched_setattr(
  */
 void cbrtDlMissHandler(int sig) {
     // BEGIN User handler
+    puts("Deadline miss!");
+    gpioTerminate();
     // END User handler
-    (void)signal(SIGXCPU, SIG_DFL);
+    (void) signal(SIGXCPU, SIG_DFL);
 }
 
 /**
@@ -162,7 +179,7 @@ void* cbrtUpdateTicksEntryPoint(void* args) {
                               .sched_runtime = UPTICK_RUNTIME, // ns
                               .sched_period = UPTICK_PERIOD, // ns
                               .sched_deadline = UPTICK_DEADLINE}; // ns
-    (void)signal(SIGXCPU, cbrtDlMissHandler);  // Register signal handler
+    (void) signal(SIGXCPU, cbrtDlMissHandler);  // Register signal handler
     if (sched_setattr(0, &attr, 0)) {
         perror("cbrtUpdateTicksEntryPoint: sched_setattr");
         exit(EXIT_FAILURE);
@@ -172,7 +189,7 @@ void* cbrtUpdateTicksEntryPoint(void* args) {
 
     for (;;) {
         // BEGIN Worker code
-        if (pthread_mutex_trylock(&tickMutex) != 0) {                                      
+        if (pthread_mutex_trylock(&ticksMutex) != 0) {                                      
     		if (errno == EBUSY) {
     			// The mutex was busy, yielding...                                                    
       			sched_yield();                            
@@ -183,13 +200,15 @@ void* cbrtUpdateTicksEntryPoint(void* args) {
     	} else {
 			// CRITICAL SECTION
     		// We have the lock on the mutex and we can update the ticks
+    		
         	ticks_L = cbEncoderLeft.ticks;
         	ticks_R = cbEncoderRight.ticks;
-        	if (pthread_mutex_unlock(&tickMutex) != 0) {                                      
+        	if (pthread_mutex_unlock(&ticksMutex) != 0) {                                      
     			perror("cbrtUpdateTicksEntryPoint: pthread_mutex_unlock");                                     
     			exit(EXIT_FAILURE);                                                                    
   			}            
     	}
+    	pthread_testcancel();
         // END Worker code
         sched_yield();
     };
@@ -228,15 +247,17 @@ void* cbrtOdoEntryPoint(void* args) {
                       (TICKS_PER_REVOLUTION * TRANSMISSION_RATIO);
     float distFromGoal_mm = DISTANCE_FROM_GOAL;
     int myTicks_L, myTicks_R;
-    int prevTicks_L, prevTicks_R;
+    int prevTicks_L = 0, prevTicks_R = 0;
     float travel_mm_L, travel_mm_R;
     //timespec_t clock;
     //tsSet(&clock);
+    cbMotorMove(&cbMotorLeft, forward, DUTY_CYC_L);
+    cbMotorMove(&cbMotorRight, forward, DUTY_CYC_R);
     // END Worker variables
 
     for (;;) {
         // BEGIN Worker code
-        if (pthread_mutex_trylock(&tickMutex) != 0) {                                      
+        if (pthread_mutex_trylock(&ticksMutex) != 0) {                                      
     		if (errno == EBUSY) {
     			// The mutex was busy, yielding...                                                    
       			sched_yield();                            
@@ -249,18 +270,24 @@ void* cbrtOdoEntryPoint(void* args) {
     		// We have the lock on the mutex and we can update the ticks
         	myTicks_L = ticks_L;
         	myTicks_R = ticks_R;
-        	if (pthread_mutex_unlock(&tickMutex) != 0) {                                      
+        	if (pthread_mutex_unlock(&ticksMutex) != 0) {                                      
     			perror("cbrtUpdateTicksEntryPoint: pthread_mutex_unlock");                                     
     			exit(EXIT_FAILURE);                                                                    
   			}            
     	}
-    	travel_mm_L = (ticks_L - prevTicks_L) * mmsPerTick_L;
-    	prevTicks_L = ticks_L;
+    	travel_mm_L = (myTicks_L - prevTicks_L) * mmsPerTick_L;
+    	prevTicks_L = myTicks_L;
     	// speed_mm_s_L = (travel_mm_L / tsTickNs(&clock)) * NSEC_PER_MSEC;	
-    	travel_mm_R = (ticks_R - prevTicks_R) * mmsPerTick_R;
-    	prevTicks_R = ticks_R;
+    	travel_mm_R = (myTicks_R - prevTicks_R) * mmsPerTick_R;
+    	prevTicks_R = myTicks_R;
         distFromGoal_mm -= (travel_mm_L + travel_mm_R) / 2;
-        if(distFromGoal_mm < .0f) break;
+		// Takes A LOT of CPU time. Do not use with low sched values.
+        //printf("TL: %f, TR: %f, DfG: %f\n", travel_mm_L, travel_mm_R, distFromGoal_mm);
+        if(distFromGoal_mm < .0f) {
+    		cbMotorReset(&cbMotorLeft);
+    		cbMotorReset(&cbMotorRight);
+        	break;
+        }
         // END Worker code
         sched_yield();
     };
@@ -272,33 +299,51 @@ task_t taskOdo = { .tid = 0, .entry = cbrtOdoEntryPoint };
 task_t taskUpdateTicks = { .tid = 0, .entry = cbrtUpdateTicksEntryPoint };
 
 int main(void) {
-    init();
-    atexit(terminate);
+    cbInit();
     // Initialize the mutex 
-    if (pthread_mutex_init(&tickMutex, NULL) != 0) {                                  
+    if (pthread_mutex_init(&ticksMutex, NULL) != 0) {                                  
     	perror("main: pthread_mutex_init");                                       
     	exit(EXIT_FAILURE);                                                                    
-  	}                                                                             
-    // Start the motors
-    cbMotorMove(&cbMotorLeft, forward, DUTY_CYC_L);
-    cbMotorMove(&cbMotorRight, forward, DUTY_CYC_R);
-    // Dispatch the tasks with pthreads
-	if(pthread_create(&(taskOdo.tid), NULL, taskOdo.entry, NULL) != 0) {
-		perror("main: pthread_create");
-		exit(EXIT_FAILURE);
+  	}
+  	{ // Create the tasks
+		if(pthread_create(&(taskOdo.tid), NULL, taskOdo.entry, NULL) != 0) {
+			perror("main: pthread_create: taskOdo");
+			exit(EXIT_FAILURE);
+		} else {
+	    	printf("main: taskOdo: Created.\n");
+		}
+		
+		if(pthread_create(&(taskUpdateTicks.tid), NULL, 
+						  taskUpdateTicks.entry, NULL) != 0) {
+			perror("main: pthread_create: taskUpdateTicks");
+			exit(EXIT_FAILURE);
+		} else {
+			printf("main: taskUpdateTicks: Created.\n");
+		}
 	}
-	if(pthread_create(&(taskUpdateTicks.tid), NULL, 
-					  taskUpdateTicks.entry, NULL) != 0) {
-		perror("main: pthread_create");
-		exit(EXIT_FAILURE);
+	{ // Wait for task completion
+	    if(pthread_join(taskOdo.tid, NULL) != 0) {
+	    	perror("main: pthread_join");
+	    	exit(EXIT_FAILURE);
+	    } else {
+	    	printf("main: taskOdo: Completed!\n");
+	    	if (pthread_cancel(taskUpdateTicks.tid) != 0) {                                            
+   				perror("main: pthread_cancel");
+   				exit(EXIT_FAILURE);
+ 			}  
+	    }
+
+	    if(pthread_join(taskUpdateTicks.tid, NULL) != 0) {
+	    	perror("main: pthread_join");
+	    	exit(EXIT_FAILURE);
+	    } else {
+	    	printf("main: taskUpdateTicks: Completed!\n");
+	    }
 	}
-    if(pthread_join(taskOdo.tid, NULL) != 0) {
-    	perror("main: pthread_join");
-    	exit(EXIT_FAILURE);
-    }
-    if(pthread_join(taskUpdateTicks.tid, NULL) != 0) {
-    	perror("main: pthread_join");
-    	exit(EXIT_FAILURE);
-    }
-    pthread_exit(EXIT_SUCCESS); // Failsafe, not really needed
+    if (pthread_mutex_destroy(&ticksMutex) != 0) {                                  
+    	perror("main: pthread_mutex_destroy");                                       
+    	exit(EXIT_FAILURE);                                                                    
+  	}
+  	cbTerminate();
+    exit(EXIT_SUCCESS);
 }
